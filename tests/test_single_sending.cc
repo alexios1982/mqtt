fbf9e966d34cd0e7b113c22fcfb80d5c836abe6e
alessandro mercadante <alexios1982@libero.it>
2019-05-09 18:45:31 +0200

    Completato il test di un invio singolo sul topic Notifications e sottoscrizione sul topic Response

--- tests/test_single_sending.cc
+++ tests/test_single_sending.cc
@@ -7,29 +7,63 @@
 #include <base64.hh> //for base64_file_converter
 #include <boost/core/ignore_unused.hpp>
 #include "Publisher_callback_listener.hh"
+#include "Test_subscriber_action_listener.hh"
 #include <iostream>
 
 const std::string PUBLISHER_SERVER_ADDRESS   { "ssl://mqtt.flespi.io:8883" };
 const std::string PUBLISHER_CLIENT_ID        { "publisher_client" };
-const std::string PUBLISHER_TOPIC            { "test_single_sending" };
+const std::string PUBLISHER_TOPIC            { "Notifications" };
+
+const std::string SUBSCRIBER_SERVER_ADDRESS {"ssl://mqtt.flespi.io:8883"};
+const std::string SUBSCRIBER_CLIENT_ID {"subscriber_client"};
+const std::string SUBSCRIBER_TOPIC {"Response"};
+
+int QOS = 1;
 
 int main(int argc, char* argv[]){
   boost::ignore_unused(argc);
   boost::ignore_unused(argv);
+
+  
+  mqtt::connect_options conn_opts{"FlespiToken R4XF03Rp3KStynVTDRrOuju7odMxQjYxdJ32DKhuiYNGbwnEbEgvMBt0C3nid9Fe", "bEEvvyn1bxDFw0-s"};
+  mqtt::ssl_options sslopts;
+  conn_opts.set_ssl(sslopts);
+
+
+  mqtt::async_client subscriber(SUBSCRIBER_SERVER_ADDRESS,
+				SUBSCRIBER_CLIENT_ID);
+  Test_subscriber_action_listener action_listener(subscriber, 
+						  conn_opts,
+						  SUBSCRIBER_TOPIC,
+						  SUBSCRIBER_CLIENT_ID,
+						  QOS);
+  subscriber.set_callback(action_listener);
+
+  // Start the connection.
+  // When completed, the callback will subscribe to topic.  
+  try {
+    std::cout << "Connecting to the SUBSCRIBER MQTT server..." << std::flush;
+    subscriber.connect(conn_opts, nullptr, action_listener);
+  }
+  catch (const mqtt::exception&) {
+    std::cerr << "\nERROR: Unable to connect to the SUBSCRIBER MQTT server: '"
+	      << SUBSCRIBER_SERVER_ADDRESS << "'" << std::endl;
+    return 1;
+  }
 
   //Create the async_client object
   mqtt::async_client publisher_client(PUBLISHER_SERVER_ADDRESS, PUBLISHER_CLIENT_ID);
   Publisher_callback publisher_callback;
   publisher_client.set_callback(publisher_callback);
   //mqtt::connect_options publisher_conn_opts{"Wb7GPA.RVtWzA", "bEEvvyn1bxDFw0-s"};
-  mqtt::connect_options publisher_conn_opts{"FlespiToken R4XF03Rp3KStynVTDRrOuju7odMxQjYxdJ32DKhuiYNGbwnEbEgvMBt0C3nid9Fe", "bEEvvyn1bxDFw0-s"};
-  mqtt::ssl_options publisher_sslopts;
-  publisher_conn_opts.set_ssl(publisher_sslopts);
 
- 
+  // mqtt::connect_options publisher_conn_opts{"FlespiToken R4XF03Rp3KStynVTDRrOuju7odMxQjYxdJ32DKhuiYNGbwnEbEgvMBt0C3nid9Fe", "bEEvvyn1bxDFw0-s"};
+  // mqtt::ssl_options publisher_sslopts;
+  // publisher_conn_opts.set_ssl(publisher_sslopts);
+
   try {
     std::cout << "\nConnecting to the PUBLISHER MQTT server..." << std::endl;
-    mqtt::token_ptr conntok = publisher_client.connect(publisher_conn_opts);
+    mqtt::token_ptr conntok = publisher_client.connect(conn_opts);
     std::cout << "Waiting for the connection..." << std::endl;
     conntok->wait();
     std::cout << "  ... CONNECTION TO PUBLISHER OK" << std::endl;
