3992903451c4666564026a5a5f4fd99ad41f3ec7
alessandro mercadante <alexios1982@libero.it>
2019-06-07 17:13:59 +0200

    Risolto il problema degli eventi multipli in caso di eventi ravvicinati:il problema deriva dal fatto che classify_message() viene lanciato in un suo thread e quindi prima che finisca l'elaborazione della notifica corrente (con la preparazione del video da mandare), potrei avere un altro evento.

--- src/Notification_logic_controller.cc
+++ src/Notification_logic_controller.cc
@@ -41,10 +41,11 @@ void Notification_logic_controller::classify_message(const mqtt::const_message_p
   auto it = _sensor_cam.find(topic_info);
   if( it != _sensor_cam.end() ){
     if( !is_a_door_sensor_notification_duplicate(message_ptr) ){
-      int iter = 0, n_of_sending = 2;
+      int iter = 0, n_of_sending = 1;
       while( iter < n_of_sending){
 	message_to_send = prepare_rich_notification(message_ptr, topic_info);
-	send_notification(message_to_send);
+	if(message_to_send->get_topic() != "")
+	  send_notification(message_to_send);
 	++iter;
       }
     }
@@ -65,6 +66,7 @@ void Notification_logic_controller::classify_message(const mqtt::const_message_p
 }
 
 mqtt::const_message_ptr Notification_logic_controller::prepare_rich_notification(const mqtt::const_message_ptr &message_ptr, const std::string &sensor_mini_id){
+  static std::string last_sent_file{};
   boost::ignore_unused(message_ptr);
   boost::property_tree::ptree pt;
   Dir_handler dir_handler{ _cam_path[ _sensor_cam[sensor_mini_id] ] };
@@ -80,22 +82,37 @@ mqtt::const_message_ptr Notification_logic_controller::prepare_rich_notification
     << "The current local time is: " << std::ctime(&now) ); //ctime adds automatically a \n  
   D( std::cout << info << "[Notification_logic_controller::" << __func__ << "]. " << reset
       << "Video " /*<< (iter + 1) <<*/" to publish is " << to_send_filename << std::endl );
-  //let's wait until the video chunk to send is finished:
-  //the condition for this to happen is the presence of a next video chunk 
-  while(true){
-    Dir_handler::Time_path_pair next = dir_handler.get_last_modified_file(".mp4");
-    if( ( (next.second).filename() ).string() != to_send_filename )
-      break;
-    std::this_thread::sleep_for ( std::chrono::milliseconds(500) ); 
+  //we take only the last 6 chars because the filename is very
+  //long and the comparison between the entire filename can be heavy
+  std::string curr_filename = to_send_filename.substr(to_send_filename.size() - 6);
+  D( std::cout << info << "[Notification_logic_controller::" << __func__ << "]. " << reset
+     << "curr: " << curr_filename << " last: " << last_sent_file << std::endl);
+  if(curr_filename != last_sent_file){
+    last_sent_file = curr_filename;
+    //let's wait until the video chunk to send is finished:
+    //the condition for this to happen is the presence of a next video chunk 
+    while(true){
+      Dir_handler::Time_path_pair next = dir_handler.get_last_modified_file(".mp4");
+      if( ( (next.second).filename() ).string() != to_send_filename )
+	break;
+      std::this_thread::sleep_for ( std::chrono::milliseconds(500) ); 
+    }
+  
+    pt.put("filename", to_send_filename);
+    pt.put( "data", base64_file_converter( (to_send.second).string() ) );
+  
+    std::stringstream ss;
+    boost::property_tree::json_parser::write_json(ss, pt);
+    //if we put assignment here, we have double sending of the same file
+    //last_sent_file = curr_filename;
+    D( std::cout << error << "[Notification_logic_controller::" << __func__ << "]. " << reset
+     << "filename: " << to_send_filename << " time: " << std::ctime(&now) << std::endl);
+    return mqtt::make_message( _publisher.get_topic(), ss.str() );
+  }else{
+    D( std::cout << info << "[Notification_logic_controller::" << __func__ << "]. " << reset
+      << "Video alredy sent" << std::endl );
+    return mqtt::make_message("", "");
   }
-  
-  pt.put("filename", to_send_filename);
-  pt.put( "data", base64_file_converter( (to_send.second).string() ) );
-  
-  std::stringstream ss;
-  boost::property_tree::json_parser::write_json(ss, pt);
-  return mqtt::make_message( _publisher.get_topic(), ss.str() );
-  
 }
 
 mqtt::const_message_ptr Notification_logic_controller::prepare_classified_notification(const mqtt::const_message_ptr &message_ptr){
@@ -144,6 +161,10 @@ void Notification_logic_controller::update_area_protection(){
 
 void Notification_logic_controller::analyze_ai_response(const mqtt::const_message_ptr &message_ptr){
   D(std::cout << warning << "[Notification_logic_controller::" << __func__ << "]. " << reset << "topic: " << message_ptr->get_topic() << '\n');
-  D(std::cout << warning << "[Notification_logic_controller::" << __func__ << "]. " << reset << "payload: " << message_ptr->get_payload() << std::endl);  
+  D(std::cout << warning << "[Notification_logic_controller::" << __func__ << "]. " << reset << "payload: " << message_ptr->get_payload() << std::endl);
+  std::time_t now;
+  std::time (&now);
+  D( std::cout << error << "[Notification_logic_controller::" << __func__ << "]. " << reset
+     << " time: " << std::ctime(&now) << std::endl);
   
 }
