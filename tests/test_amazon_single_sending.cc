3751897be9ca6880293f9bc1271ddf99b6ad7c1b
alessandro mercadante <alexios1982@libero.it>
2019-06-24 14:54:46 +0200

    Aggiunto il test test_image_extraction_with_opencv.cc per verificare le performace di opencv nel ritagliare una singola immagine jpeg dal filmato h264

--- tests/test_amazon_single_sending.cc
+++ tests/test_amazon_single_sending.cc
@@ -55,9 +55,11 @@ int main(int argc, char* argv[]){
   }
 
   boost::property_tree::ptree pt;
-  pt.put("filename", "2019-06-19_18:20:56_cam01_4.mp4");
-  pt.put( "data", base64_file_converter("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-06-19_18:20:56_cam01_4.mp4") );
+  //pt.put("filename", "2019-06-19_18:20:56_cam01_4.mp4");
+  pt.put("filename", "2019-04-30_14:46:22_cam02_5.jpeg");
+  //pt.put( "data", base64_file_converter("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-06-19_18:20:56_cam01_4.mp4") );
   //pt.put( "data", base64_file_converter("/home/pi/mqtt/mqtt/temp_saved_file_dir/ex_file_300x400.jpg") );
+  pt.put( "data", base64_file_converter("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-04-30_14:46:22_cam02_5.jpeg") );
   std::stringstream ss;
   boost::property_tree::json_parser::write_json(ss, pt);
   mqtt::message_ptr pubmsg = mqtt::make_message( PUBLISHER_TOPIC, ss.str() );
---
+++ tests/test_image_extraction_with_opencv.cc
@@ -0,0 +1,43 @@
+//this test is intended to show the performance in terms of time of Opencv to extract a single jpg image from an mp4 video
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Utils.hh"
+
+int main(int argc, char **argv) {
+  Time_spent<> time_spent{};
+  cv::VideoCapture cap("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-06-24_14:39:34_cam02_5.mp4"); // video
+  if ( !cap.isOpened() ){
+    std::cerr << "Cannot open the video file" << std::endl;
+    return 1;
+  }
+
+  double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
+  D( std::cout << "Frame per seconds : " << fps << std::endl );
+
+  cv::Mat frame;
+  bool bSuccess = cap.read(frame); // read a new frame from video
+  
+  if (!bSuccess){
+    std::cerr << "Cannot read the frame from video file" << std::endl;
+    return 1;
+  }
+  imwrite("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-06-24_14:39:34_cam02_5_opencv_start.jpg", frame);
+
+  //we can set a specific time in the video to extract the image from
+  cap.set(CV_CAP_PROP_POS_MSEC, 2800);
+  bSuccess = cap.read(frame);
+
+  if (!bSuccess){
+    std::cerr << "Cannot read the frame from video file" << std::endl;
+    return 1;
+  }
+
+  imwrite("/home/pi/mqtt/mqtt/temp_saved_file_dir/2019-06-24_14:39:34_cam02_5_opencv_end.jpg", frame);
+
+  return 0;
+  
+}
